c6b5c64fb3f65b263cf84010d9ebcfc2870845b4
eojin312 <eojin312@naver.com>
2024-11-24 16:10:10 +0900

    copy 본 쉘카피를 이용해서 값 변경하기

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ import {useState} from "react";
 
 function App() {
     let post = '맛집';
-    let [title1, title2] = useState(['남자 코트 추천', '여성 코트 추천']);
+    let [title1, actionChangeTitle] = useState(['파이썬',  '자바', '코틀린', '루비']);
     let [logo, setLogo] = useState('title');
     let [popupApply, good] = useState(0);
-    function plusOne() {
-        console.log(1);
-        popupApply = popupApply + 1;
-    }
 
     return (
         <div className="App">
@@ -20,7 +16,19 @@ function App() {
                 <h4>{logo}</h4>
             </div>
             <div className="list">
-                <h4>{ title1[0] } <button onClick={ () => {good(popupApply+1)} }>좋아요</button> {popupApply} </h4>
+                <button onClick={() => {
+                    let copy = [...title1]
+                    copy[0] = '여성 코트 추천'
+                    actionChangeTitle(copy);
+                }}>글 수정
+                </button>
+
+                <h4>{title1[0]}
+                    <button onClick={() => {
+                        good(popupApply + 1)
+                    }}>좋아요
+                    </button>
+                    {popupApply} </h4>
                 <p>2/18 발행</p>
             </div>
             <div className="list">
