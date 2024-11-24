f273e25762d19dd66adf81fc360252288cb143ce
이어진 <ejlee@hanatour.com>
2024-11-24 15:32:27 +0900

    onClick 배우기, useState() 를 이용하여 유동적인 데이터 랜더링

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let post = '맛집';
+    let [title1, title2] = useState(['남자 코트 추천', '여성 코트 추천']);
+    let [logo, setLogo] = useState('title');
+    let [popupApply, good] = useState(0);
+    function plusOne() {
+        console.log(1);
+        popupApply = popupApply + 1;
+    }
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>{logo}</h4>
+            </div>
+            <div className="list">
+                <h4>{ title1[0] } <button onClick={ () => {good(popupApply+1)} }>좋아요</button> {popupApply} </h4>
+                <p>2/18 발행</p>
+            </div>
+            <div className="list">
+                <h4>{title1[1]}</h4>
+                <p>2/18 발행</p>
+            </div>
+            <div className="list">
+                <h4>{title1[1]}</h4>
+                <p>2/18 발행</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
